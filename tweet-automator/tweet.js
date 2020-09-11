10feab6231d692d616b4cce30e07138e0454e30c
Brian Childress <brian@brianchildress.co>
2020-09-11 17:20:09 -0400

    Trying to get secrets working

--- tweet-automator/tweet.js
+++ tweet-automator/tweet.js
@@ -10,7 +10,7 @@ const client = new Twitter({
   access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
-console.log(client.consumer_key);
+console.log(client.consumer_key, process.env.API_KEY);
 
 // get RSS feed
 const parser = new Parser();
